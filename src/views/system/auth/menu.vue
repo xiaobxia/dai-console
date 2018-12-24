0206da60e06b8380f54263034fa1911ea4125b5c
xiaobxia <673806687@qq.com>
2018-12-24 14:05:05 +0800

    xg

--- src/views/system/auth/menu.vue
+++ src/views/system/auth/menu.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-button class="filter-item" icon="el-icon-plus" type="primary">新增</el-button>
+      <el-button class="filter-item" icon="el-icon-plus" type="primary" @click="handleCreate">新增</el-button>
     </div>
+    <el-dialog :visible.sync="dialogFormVisible" title="添加菜单">
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="handleClose">确定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -13,11 +18,23 @@ export default {
   components: { },
   data() {
     return {
+      dialogFormVisible: false,
+      addForm: {
+
+      }
     }
   },
   computed: {
   },
   created() {
+  },
+  methods: {
+    handleCreate() {
+      this.dialogFormVisible = true
+    },
+    handleClose() {
+      this.dialogFormVisible = false
+    }
   }
 }
 </script>

faed233c7c57ff92d7d1860a8aac14f88284a497
xiaobxia <673806687@qq.com>
2018-12-24 14:27:01 +0800

    xg

--- src/views/system/auth/menu.vue
+++ src/views/system/auth/menu.vue
@@ -3,7 +3,12 @@
     <div class="filter-container">
       <el-button class="filter-item" icon="el-icon-plus" type="primary" @click="handleCreate">新增</el-button>
     </div>
-    <el-dialog :visible.sync="dialogFormVisible" title="添加菜单">
+    <el-dialog :visible.sync="dialogFormVisible" :title="ifAddDialogForm ? '添加菜单':'修改菜单'">
+      <el-form ref="form" :model="dialogForm" label-position="left" label-width="80px">
+        <el-form-item label="菜单名称">
+          <el-input v-model="dialogForm.name"/>
+        </el-form-item>
+      </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button type="primary" @click="handleClose">确定</el-button>
       </span>
@@ -13,18 +18,24 @@
 
 <script>
 
+const dialogFormBase = {
+  name: ''
+}
+
 export default {
   name: 'SystemAuthMenu',
   components: { },
   data() {
     return {
       dialogFormVisible: false,
-      addForm: {
-
-      }
+      dialogFormStatus: 'add',
+      dialogForm: Object.assign({}, dialogFormBase)
     }
   },
   computed: {
+    ifAddDialogForm() {
+      return this.dialogFormStatus === 'add'
+    }
   },
   created() {
   },
@@ -34,6 +45,7 @@ export default {
     },
     handleClose() {
       this.dialogFormVisible = false
+      this.dialogForm = Object.assign({}, dialogFormBase)
     }
   }
 }

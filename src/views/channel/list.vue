3454d74d747993cc9dd63cdf6701a2ea955c46c7
xiaobxia <673806687@qq.com>
2019-01-02 18:13:38 +0800

    xg

--- src/views/channel/list.vue
+++ src/views/channel/list.vue
@@ -203,6 +203,7 @@ export default {
           type: 'success',
           message: '删除成功!'
         })
+        this.initPage()
       })
     },
     closeForm() {
--- src/views/user/manger/bankList.vue
+++ src/views/user/manger/bankList.vue
@@ -143,6 +143,7 @@
 
 <script>
 import Pagination from '@/components/Pagination'
+import moment from 'moment'
 const searchFormBase = {
   userId: '',
   userName: '',
@@ -207,10 +208,10 @@ export default {
       for (const key in this.searchForm) {
         if (key === 'time') {
           if (this.searchForm['time'][0]) {
-            data.beginTime = this.searchForm.time[0]
-            data.endTime = this.searchForm.time[1]
-            // data.beginTime = moment(this.searchForm.time[0]).format('YYYY-MM-DD')
-            // data.endTime = moment(this.searchForm.time[1]).format('YYYY-MM-DD')
+            // data.beginTime = this.searchForm.time[0]
+            // data.endTime = this.searchForm.time[1]
+            data.beginTime = moment(this.searchForm.time[0]).format('YYYY-MM-DD')
+            data.endTime = moment(this.searchForm.time[1]).format('YYYY-MM-DD')
           }
         } else {
           data[key] = this.searchForm[key]
